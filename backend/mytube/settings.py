44a3f9808e9ec18dd81a452daf26b691828c731e
Kaya Luken <kayaluken@gmail.com>
2025-04-26 18:29:30 +0200

    feat: initial drf scaffolding

--- backend/mytube/settings.py
+++ backend/mytube/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    'rest_framework',
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
